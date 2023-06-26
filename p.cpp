9d45e1313b71a0fcda500ce7d9b782ca178edae3
Ftm-jfr <127336618+Ftm-jfr@users.noreply.github.com>
2023-06-26 19:21:27 +0330

    delete product function
    It deletes the product file and its changes in the general product file

--- p.cpp
+++ p.cpp
@@ -27,6 +27,7 @@ public:
         products<< this->name<<endl<< this->Product_ID<<endl;
         products.close();
     }
+
     void updatequantity(int newnum,const string& namee)
     {
         ifstream product((namee + ".txt"));
@@ -49,5 +50,34 @@ public:
         remove((namee + ".txt").c_str());
         rename("temp.txt",(namee + ".txt").c_str());
     }
+
+    void deleteproduct()
+    {
+        cout<<"Enter the product name :";
+        cin>>name;
+        remove((name+(".txt")).c_str());
+        ofstream temp("temp.txt");
+        ifstream product("products.txt");
+        string line;
+        while (getline(product,line)){
+            if(line!=name)
+            {
+                temp<<line<<endl;
+                getline(product,line);
+                temp<<line<<endl;
+            }
+            else
+                getline(product,line);
+        }
+        product.close();
+        temp.close();
+
+        remove("products.txt");
+        rename("temp.txt","products.txt");
+    }
+
+
+
+
 
 };
